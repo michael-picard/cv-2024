bbfa61a43423670e267fb950f9fd6227967b3aab
michael <michael@go2roues.com>
2024-06-17 08:43:06 +0200

    Added quick pres buttons

--- components/QuickPres.vue
+++ components/QuickPres.vue
@@ -1,18 +1,26 @@
 <template>
   <section class="card">
-      <div class="quick-pres-grid card__content">
+    <div class="quick-pres__grid card__content">
+      <div>
+        <img
+            src="https://placehold.co/250x300"
+            alt=""
+            width="250"
+            height="300"
+        >
         <div>
-          <img
-              src="https://placehold.co/250x300"
-              alt=""
-          >
-          <div><h1>Michael Picard</h1></div>
-          <div>Full stack web developer</div>
-        </div>
-        <div>
-          <p>Hello, my friends and colleagues call me Mike. I am a French senior full stack developer with over 10 years of experience in the e-commerce industry. I obsess over web perfomance and thrive on constant learning. My skills and experience will help you solve problems and make more money.</p>
+          <h1>Michael Picard</h1>
+          Full stack web developer
         </div>
       </div>
+      <div >
+        <p>Hello, my friends and colleagues call me Mike. I am a French senior full stack developer with over 10 years of experience in the e-commerce industry. I obsess over web perfomance and thrive on constant learning. My skills and experience will help you solve problems and make more money.</p>
+        <div class="quick-pres__actions">
+          <a href="https://www.linkedin.com/in/michael-picard-a7607540/" target="_blank" aria-label="Mon profil Linkedin"><LinkedinIcon :size="30"/></a>
+          <a href="tel:+33658340416" aria-label="Me contacter par téléphone"><PhoneIcon :size="30" /></a>
+        </div>
+      </div>
+    </div>
   </section>
 </template>
 
@@ -21,17 +29,29 @@
     lang="ts"
 >
 
+import LinkedinIcon from "~/components/icons/LinkedinIcon.vue";
+import PhoneIcon from "~/components/icons/PhoneIcon.vue";
 </script>
 
 <style
     scoped
     lang=scss
 >
-.quick-pres-grid {
+.quick-pres__grid {
   display: grid;
   grid-template-columns: 1fr 2fr;
   place-items: center;
   grid-gap: 2rem;
-  //background-image: url("/endless-constellation.svg");
+
+  > div:first-child {
+    text-align: center;
+  }
+
+  .quick-pres__actions {
+    display: flex;
+    gap: 1rem;
+    margin-top: 1rem;
+    justify-content: center;
+  }
 }
 </style>
---
+++ components/icons/PhoneIcon.vue
@@ -0,0 +1,42 @@
+<template>
+  <svg
+      xmlns="http://www.w3.org/2000/svg"
+      :width="size"
+      :height="size"
+      viewBox="0 0 48 48"
+  ><title>{{title}}</title>
+    <rect
+        x="0"
+        y="0"
+        width="48"
+        height="48"
+        rx="0"
+        ry="0"
+        stroke="none"
+        :fill="fill"
+    ></rect>
+    <g
+        fill="white"
+        transform="translate(7.2 7.2) scale(0.7)"
+    >
+      <path
+          d="M31.376,29.175,27.79,33.658A37.827,37.827,0,0,1,14.343,20.212l4.482-3.586a3.044,3.044,0,0,0,.88-3.614l-4.086-9.2A3.047,3.047,0,0,0,12.067,2.1L4.29,4.116A3.063,3.063,0,0,0,2.029,7.5,45.2,45.2,0,0,0,40.5,45.972a3.064,3.064,0,0,0,3.384-2.261L45.9,35.932a3.049,3.049,0,0,0-1.712-3.551L34.99,28.3A3.046,3.046,0,0,0,31.376,29.175Z"
+      ></path>
+    </g>
+  </svg>
+</template>
+
+<script
+    setup
+    lang="ts"
+>
+withDefaults(defineProps<{
+  fill?: string,
+  title?: string,
+  size?: number,
+}>(), {
+  fill: 'currentColor',
+  title: '',
+  size: 18,
+})
+</script>
