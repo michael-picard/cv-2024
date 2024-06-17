fc4d0faa282915ba957a22502303d5cd72259cbf
michael <michael@go2roues.com>
2024-06-17 08:44:01 +0200

    Animated the progress bars

--- components/Skills.vue
+++ components/Skills.vue
@@ -2,13 +2,13 @@
   <section class="card">
     <div class="card__content">
       <h2>Skills</h2>
-      <div class="skills-grid">
+      <div class="skills__grid">
         <div>
           <h3>
             <WindowCodeIcon/>
             Programming
           </h3>
-          <ul>
+          <ul class="skills__languages">
             <li
                 v-for="language in languages"
                 :key="language.name"
@@ -17,6 +17,7 @@
               <v-progress-linear
                   color="primary"
                   :model-value="language.level"
+                  :buffer-value="language.buffer"
               ></v-progress-linear>
             </li>
           </ul>
@@ -26,7 +27,7 @@
             <FrameworkIcon/>
             Frameworks
           </h3>
-          <ul>
+          <ul class="skills__frameworks">
             <li
                 v-for="framework in frameworks"
                 :key="framework.name"
@@ -35,6 +36,7 @@
               <v-progress-linear
                   color="primary"
                   :model-value="framework.level"
+                  :buffer-value="framework.buffer"
               ></v-progress-linear>
             </li>
           </ul>
@@ -44,7 +46,7 @@
             <DatabaseIcon/>
             Databases
           </h3>
-          <ul>
+          <ul class="skills__databases">
             <li
                 v-for="database in databases"
                 :key="database.name"
@@ -53,6 +55,7 @@
               <v-progress-linear
                   color="primary"
                   :model-value="database.level"
+                  :buffer-value="database.buffer"
               ></v-progress-linear>
             </li>
           </ul>
@@ -82,41 +85,73 @@ import FrameworkIcon from "~/components/icons/FrameworkIcon.vue";
 import DatabaseIcon from "~/components/icons/DatabaseIcon.vue";
 import ToolsIcon from "~/components/icons/ToolsIcon.vue";
 
-const languages = [
-  {name: "JavaScript", level: 95},
-  {name: "TypeScript", level: 90},
-  {name: "Node, Deno", level: 85},
-  {name: "PHP", level: 95},
-  {name: "HTML", level: 95},
-  {name: "CSS", level: 90},
-  {name: "GO", level: 15},
-  {name: "Docker", level: 20},
-];
+const languages = ref([
+  {name: "JavaScript", level: 0, buffer: 95},
+  {name: "TypeScript", level: 0, buffer: 90},
+  {name: "Node, Deno", level: 0, buffer: 85},
+  {name: "PHP", level: 0, buffer: 95},
+  {name: "HTML", level: 0, buffer: 95},
+  {name: "CSS", level: 0, buffer: 90},
+  {name: "GO", level: 0, buffer: 15},
+  {name: "Docker", level: 0, buffer: 20},
+]);
 
-const frameworks = [
-  {name: "Vue", level: 95},
-  {name: "Nuxt", level: 70},
-  {name: "Woocommerce", level: 95},
-  {name: "Laravel", level: 80},
-  {name: "Shopify", level: 60},
-  {name: "Magento", level: 30},
-];
+const frameworks = ref([
+  {name: "Vue", level: 0, buffer: 95},
+  {name: "Nuxt", level: 0, buffer: 70},
+  {name: "Woocommerce", level: 0, buffer: 95},
+  {name: "Laravel", level: 0, buffer: 80},
+  {name: "Shopify", level: 0, buffer: 60},
+  {name: "Magento", level: 0, buffer: 30},
+]);
 
-const databases = [
-  {name: "Mysql, Mariadb", level: 95},
-  {name: "SQL Server", level: 95},
-  {name: "MongoDB", level: 60},
-  {name: "Redis", level: 40},
-];
+const databases = ref([
+  {name: "Mysql, Mariadb", level: 0, buffer: 95},
+  {name: "SQL Server", level: 0, buffer: 95},
+  {name: "MongoDB", level: 0, buffer: 60},
+  {name: "Redis", level: 0, buffer: 40},
+]);
+
+const skills = reactive({
+  languages,
+  frameworks,
+  databases,
+})
+
+onMounted(() => {
+  const observerOptions: IntersectionObserverInit = {rootMargin: "0px 0px 0px 0px"}
+
+  const ratedSkills = document.querySelectorAll('.skills__grid ul[class^="skills"]')
+  ratedSkills.forEach(ul => {
+    const property = ul.classList[0].split('__')[1]
+    const observer = new IntersectionObserver((entries) => {
+      entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+          skills[property].forEach((skill, i) => {
+            setTimeout(() => {
+              skill.level = skill.buffer
+            }, 15 * i)
+          })
+        } else {
+          skills[property].forEach(database => {
+            database.level = 0
+          })
+        }
+      })
+    }, observerOptions)
+    observer.observe(ul)
+  })
+})
+
 </script>
 
 <style
     scoped
     lang=scss
 >
-.skills-grid {
+.skills__grid {
   display: grid;
-  grid-template-columns: 1fr 1fr;
+  grid-template-columns: repeat(auto-fit, minmax(300px,1fr));
   gap: 2rem;
 
   h3 {
